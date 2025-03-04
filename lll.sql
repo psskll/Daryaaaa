509b2a56f563db1239c654d853f079f7276a5d5f
psskll <160572644+psskll@users.noreply.github.com>
2025-03-04 20:53:15 +0700

    Add files via upload

---
+++ lll.sql
@@ -0,0 +1,25 @@
+-- Создаем роль с правами на создание баз данных и добавление данных
+CREATE ROLE 'lu4dan';
+
+-- Предоставляем роли права на создание баз данных
+GRANT CREATE ON *.* TO 'lu4dan';
+
+-- Предоставляем роли права на изменение структуры таблиц в базе данных apteka
+GRANT ALTER ON apteka.* TO 'lu4dan'; -- Добавлена эта строка
+
+-- Назначаем привилегии роли для работы с данными в базе данных apteka
+GRANT SELECT, INSERT, UPDATE, DELETE ON apteka.* TO 'lu4dan';
+
+-- Создаем пользователя
+CREATE USER 'dasss'@'localhost' IDENTIFIED BY '123456987!';
+
+-- Устанавливаем пароль на неистечение (опционально)
+ALTER USER 'dasss'@'localhost' PASSWORD EXPIRE NEVER;
+
+-- Назначаем роль пользователю
+GRANT 'lu4dan' TO 'dasss'@'localhost';
+
+-- Устанавливаем роль по умолчанию для пользователя
+SET DEFAULT ROLE 'lu4dan' TO 'dasss'@'localhost';
+
+FLUSH PRIVILEGES; -- Применяем изменения
